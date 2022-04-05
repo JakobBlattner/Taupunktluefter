67459031e322a951ae6d718c2833e0e899811694
JakobBlattner <40232144+JakobBlattner@users.noreply.github.com>
2022-04-05 10:06:25 +0200

    added additional output, rearranged code to work for/with "fan always on" switch

--- Taupunkt_Lueftung.ino
+++ Taupunkt_Lueftung.ino
@@ -4,21 +4,23 @@
 #include <LiquidCrystal_I2C.h>
 #include <avr/wdt.h>
 
-#define RELAIPIN 6 // Anschluss des Lüfter-Relais
-#define DHTPIN_1 5 // Datenleitung für den DHT-Sensor 1 (innen)
-#define DHTPIN_2 4 // Datenleitung für den DHT-Sensor 2 (außen)
+#define RELAIPIN 7 // Anschluss des Lüfter-Relais
+#define DHTPIN_1 4 // Datenleitung für den DHT-Sensor 1 (innen)
+#define DHTPIN_2 5 // Datenleitung für den DHT-Sensor 2 (außen)
+#define ALWAYS_ON_IN 2
+#define ALWAYS_ON_OUT 3
 
-#define RELAIS_EIN LOW
-#define RELAIS_AUS HIGH
+#define RELAIS_EIN HIGH
+#define RELAIS_AUS LOW
 bool rel;
 
-#define DHTTYPE_1 DHT22 // DHT 22 
-#define DHTTYPE_2 DHT22 // DHT 22  
+#define DHTTYPE_1 DHT22 // DHT 22
+#define DHTTYPE_2 DHT22 // DHT 22
 
 // *******  Korrekturwerte der einzelnen Sensorwerte  *******
 #define Korrektur_t_1  -3 // Korrekturwert Innensensor Temperatur
 #define Korrektur_t_2  -4 // Korrekturwert Außensensor Temperatur
-#define Korrektur_h_1  0  // Korrekturwert Innensensor Luftfeuchtigkeit
+#define Korrektur_h_1  0  // Korrekturwert Innensensor Luftfeuchtigkeit<
 #define Korrektur_h_2  0  // Korrekturwert Außensensor Luftfeuchtigkeit
 //***********************************************************
 
@@ -36,6 +38,9 @@ bool fehler = true;
 
 void setup() {
   wdt_enable(WDTO_8S); // Watchdog timer auf 8 Sekunden stellen
+  pinMode(ALWAYS_ON_IN, INPUT);
+  pinMode(ALWAYS_ON_OUT, OUTPUT);
+  digitalWrite(ALWAYS_ON_OUT, HIGH);
   
   pinMode(RELAIPIN, OUTPUT);          // Relaispin als Output definieren
   digitalWrite(RELAIPIN, RELAIS_AUS); // Relais ausschalten
@@ -59,27 +64,36 @@ void setup() {
 
 void loop() {
 
-  float h1 = dht1.readHumidity()+Korrektur_h_1;       // Innenluftfeuchtigkeit auslesen und unter „h1“ speichern
-  float t1 = dht1.readTemperature()+ Korrektur_t_1;   // Innentemperatur auslesen und unter „t1“ speichern
-  float h2 = dht2.readHumidity()+Korrektur_h_2;       // Außenluftfeuchtigkeit auslesen und unter „h2“ speichern
-  float t2 = dht2.readTemperature()+ Korrektur_t_2;   // Außentemperatur auslesen und unter „t2“ speichern
-  
-  if (fehler == true)  // Prüfen, ob gültige Werte von den Sensoren kommen
-  {
-    fehler = false; 
-    if (isnan(h1) || isnan(t1) || h1 > 100 || h1 < 1 || t1 < -40 || t1 > 80 )  {
-      Serial.println(F("Fehler beim Auslesen vom 1. Sensor!"));
-      lcd.setCursor(0,1);
-      lcd.print(F("Fehler Sensor 1"));
-      fehler = true;
-    }else {
-     lcd.setCursor(0,1);
-     lcd.print(F("Sensor 1 in Ordnung"));
-   }
-  
-    delay(2000);  // Zeit um das Display zu lesen
-  
-      if (isnan(h2) || isnan(t2) || h2 > 100 || h2 < 1 || t2 < -40 || t2  > 80)  {
+  bool fan_always_on = !digitalRead(ALWAYS_ON_IN);
+
+  Serial.print("fan_always_on is ");
+  Serial.println(fan_always_on);
+  Serial.println();
+
+  if(fan_always_on == false){
+
+    float h1 = dht1.readHumidity()+Korrektur_h_1;       // Innenluftfeuchtigkeit auslesen und unter „h1“ speichern
+    float t1 = dht1.readTemperature()+ Korrektur_t_1;   // Innentemperatur auslesen und unter „t1“ speichern
+
+    float h2 = dht2.readHumidity()+Korrektur_h_2;       // Außenluftfeuchtigkeit auslesen und unter „h2“ speichern
+    float t2 = dht2.readTemperature()+ Korrektur_t_2;   // Außentemperatur auslesen und unter „t2“ speichern
+    
+    if (fehler == true)  // Prüfen, ob gültige Werte von den Sensoren kommen
+    {
+      fehler = false; 
+      if (isnan(h1) || isnan(t1) || h1 > 100 || h1 < 1 || t1 < -40 || t1 > 80 ){
+        Serial.println(F("Fehler beim Auslesen vom 1. Sensor!"));
+        lcd.setCursor(0,1);
+        lcd.print(F("Fehler Sensor 1"));
+        fehler = true;
+      }else {
+        lcd.setCursor(0,1);
+        lcd.print(F("Sensor 1 in Ordnung"));
+      }
+    
+      delay(2000);  // Zeit um das Display zu lesen
+    
+      if (isnan(h2) || isnan(t2) || h2 > 100 || h2 < 1 || t2 < -40 || t2  > 80){
         Serial.println(F("Fehler beim Auslesen vom 2. Sensor!"));
         lcd.setCursor(0,2);
         lcd.print(F("Fehler Sensor 2"));
@@ -87,116 +101,130 @@ void loop() {
       } else {
         lcd.setCursor(0,2);
         lcd.print(F("Sensor 2 in Ordnung"));
-     }
-
-    delay(2000);  // Zeit um das Display zu lesen
-  }
-  if (isnan(h1) || isnan(t1) || isnan(h2) || isnan(t2)) fehler = true;
-   
- if (fehler == true) {
-    digitalWrite(RELAIPIN, RELAIS_AUS); // Relais ausschalten 
+      }
+  
+      delay(2000);  // Zeit um das Display zu lesen
+    }
+    if (isnan(h1) || isnan(t1) || isnan(h2) || isnan(t2)) 
+      fehler = true;
+     
+    if (fehler == true) {
+      digitalWrite(RELAIPIN, RELAIS_AUS); // Relais ausschalten 
+      lcd.setCursor(0,3);
+      lcd.print(F("CPU Neustart....."));
+      while (1);  // Endlosschleife um das Display zu lesen und die CPU durch den Watchdog neu zu starten
+    }
+    wdt_reset();  // Watchdog zurücksetzen
+  
+    //**** Taupunkte errechnen********
+    float Taupunkt_1 = taupunkt(t1, h1);
+    float Taupunkt_2 = taupunkt(t2, h2);
+  
+  
+    // Werteausgabe auf Serial Monitor
+    Serial.print(F("Sensor-1: " ));
+    Serial.print(F("Luftfeuchtigkeit: "));
+    Serial.print(h1);                     
+    Serial.print(F("%  Temperatur: "));
+    Serial.print(t1);
+    Serial.print(F("°C  "));
+    Serial.print(F("  Taupunkt: "));
+    Serial.print(Taupunkt_1);
+    Serial.println(F("°C  "));
+  
+    Serial.print("Sensor-2: " );
+    Serial.print(F("Luftfeuchtigkeit: "));
+    Serial.print(h2);
+    Serial.print(F("%  Temperatur: "));
+    Serial.print(t2);
+    Serial.print(F("°C "));
+    Serial.print(F("   Taupunkt: "));
+    Serial.print(Taupunkt_2);
+    Serial.println(F("°C  "));
+  
+    // Werteausgabe auf dem I2C-Display
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(F("S1: "));
+    lcd.print(t1); 
+    lcd.write((uint8_t)0); // Sonderzeichen °C
+    lcd.write(('C'));
+    lcd.write((uint8_t)1); // Sonderzeichen |
+    lcd.print(h1);
+    lcd.print(F(" %"));
+  
+    lcd.setCursor(0,1);
+    lcd.print(F("S2: "));
+    lcd.print(t2); 
+    lcd.write((uint8_t)0); // Sonderzeichen °C
+    lcd.write(('C'));
+    lcd.write((uint8_t)1); // Sonderzeichen |
+    lcd.print(h2);
+    lcd.print(F(" %"));
+  
+    lcd.setCursor(0,2);
+    lcd.print(F("Taupunkt 1: "));
+    lcd.print(Taupunkt_1); 
+    lcd.write((uint8_t)0); // Sonderzeichen °C
+    lcd.write(('C'));
+  
     lcd.setCursor(0,3);
-    lcd.print(F("CPU Neustart....."));
-    while (1);  // Endlosschleife um das Display zu lesen und die CPU durch den Watchdog neu zu starten
- }
- wdt_reset();  // Watchdog zurücksetzen
-
-//**** Taupunkte errechnen********
-float Taupunkt_1 = taupunkt(t1, h1);
-float Taupunkt_2 = taupunkt(t2, h2);
-
-// Werteausgabe auf Serial Monitor
- Serial.print(F("Sensor-1: " ));
-  Serial.print(F("Luftfeuchtigkeit: "));
-  Serial.print(h1);                     
-  Serial.print(F("%  Temperatur: "));
-  Serial.print(t1);
-  Serial.print(F("°C  "));
-  Serial.print(F("  Taupunkt: "));
-  Serial.print(Taupunkt_1);
-  Serial.println(F("°C  "));
-
-  Serial.print("Sensor-2: " );
-  Serial.print(F("Luftfeuchtigkeit: "));
-  Serial.print(h2);
-  Serial.print(F("%  Temperatur: "));
-  Serial.print(t2);
-  Serial.print(F("°C "));
-  Serial.print(F("   Taupunkt: "));
-  Serial.print(Taupunkt_2);
-  Serial.println(F("°C  "));
-
-
- Serial.println();
-
-  // Werteausgabe auf dem I2C-Display
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(F("S1: "));
-  lcd.print(t1); 
-  lcd.write((uint8_t)0); // Sonderzeichen °C
-  lcd.write(('C'));
-  lcd.write((uint8_t)1); // Sonderzeichen |
-  lcd.print(h1);
-  lcd.print(F(" %"));
-
-  lcd.setCursor(0,1);
-  lcd.print(F("S2: "));
-  lcd.print(t2); 
-  lcd.write((uint8_t)0); // Sonderzeichen °C
-  lcd.write(('C'));
-  lcd.write((uint8_t)1); // Sonderzeichen |
-  lcd.print(h2);
-  lcd.print(F(" %"));
-
-  lcd.setCursor(0,2);
-  lcd.print(F("Taupunkt 1: "));
-  lcd.print(Taupunkt_1); 
-  lcd.write((uint8_t)0); // Sonderzeichen °C
-  lcd.write(('C'));
-
-  lcd.setCursor(0,3);
-  lcd.print(F("Taupunkt 2: "));
-  lcd.print(Taupunkt_2); 
-  lcd.write((uint8_t)0); // Sonderzeichen °C
-  lcd.write(('C'));
-
-delay(6000); // Zeit um das Display zu lesen
-wdt_reset(); // Watchdog zurücksetzen
-
-  lcd.clear();
-  lcd.setCursor(0,0);
+    lcd.print(F("Taupunkt 2: "));
+    lcd.print(Taupunkt_2); 
+    lcd.write((uint8_t)0); // Sonderzeichen °C
+    lcd.write(('C'));
   
-float DeltaTP = Taupunkt_1 - Taupunkt_2;
+    delay(8000); // Zeit um das Display zu lesen
+    wdt_reset(); // Watchdog zurücksetzen
+  
+    lcd.clear();
+    lcd.setCursor(0,0);
+    
+    float DeltaTP = Taupunkt_1 - Taupunkt_2;
+  
+    if (DeltaTP > (SCHALTmin + HYSTERESE))rel = true;
+    if (DeltaTP < (SCHALTmin))rel = false;
+    if (t1 < TEMP1_min )rel = false;
+    if (t2 < TEMP2_min )rel = false;
 
-if (DeltaTP > (SCHALTmin + HYSTERESE))rel = true;
-if (DeltaTP < (SCHALTmin))rel = false;
-if (t1 < TEMP1_min )rel = false;
-if (t2 < TEMP2_min )rel = false;
+    if (rel == true)
+    {
+      digitalWrite(RELAIPIN, RELAIS_EIN); // Relais einschalten
+      lcd.print(F("Lueftung AN"));  
+    } else {                             
+      digitalWrite(RELAIPIN, RELAIS_AUS); // Relais ausschalten
+      lcd.print(F("Lueftung AUS"));
+    }
+  
+    lcd.setCursor(0,1);
+    lcd.print("Delta TP: ");
+    lcd.print(DeltaTP);
+    lcd.write((uint8_t)0); // Sonderzeichen °C
+    lcd.write('C');
 
-if (rel == true)
-{
-  digitalWrite(RELAIPIN, RELAIS_EIN); // Relais einschalten
-  lcd.print(F("Lueftung AN"));  
-} else {                             
-  digitalWrite(RELAIPIN, RELAIS_AUS); // Relais ausschalten
-  lcd.print(F("Lueftung AUS"));
+    int interval = 4000;
+    long startTime = millis();
+    while(millis() - startTime < interval){
+      lcd.setCursor(0,2);
+      lcd.print(F("Naechste Messung:"));
+      lcd.setCursor(0,3);
+      lcd.print(String(interval/1000 - ((millis() - startTime)/1000)) + " Sek.");
+      delay(100);
+    }
+    wdt_reset();   // Watchdog zurücksetzen 
+  }
+  else{ //activating relais, if fan switch is on "always on"
+    digitalWrite(RELAIPIN, RELAIS_EIN); // Relais einschalten
+    lcd.clear();
+    lcd.print(F("Lueftung IMMER AN"));
+
+    delay(2000);
+    wdt_reset();
+  } 
 }
 
- lcd.setCursor(0,1);
- lcd.print("Delta TP: ");
- lcd.print(DeltaTP);
- lcd.write((uint8_t)0); // Sonderzeichen °C
- lcd.write('C');
-
- delay(4000);   // Wartezeit zwischen zwei Messungen
- wdt_reset();   // Watchdog zurücksetzen 
- 
-}
-
-float taupunkt(float t, float r) {
-  
-float a, b;
+float taupunkt(float t, float r) {  
+  float a, b;
   
   if (t >= 0) {
     a = 7.5;
@@ -221,7 +249,7 @@ float a, b;
 }
 
 
- void software_Reset() // Startet das Programm neu, nicht aber die Sensoren oder das LCD 
-  {
-    asm volatile ("  jmp 0");  
-  }
+void software_Reset() // Startet das Programm neu, nicht aber die Sensoren oder das LCD 
+{
+  asm volatile ("  jmp 0");  
+}
